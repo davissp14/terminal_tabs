b6cdee165ecb548517a0758df40a9aafa54d98ad
shaun <davissp14@gmail.com>
2012-10-08 01:45:25 -0700

    Modified generation success message.

--- terminal_jobs.rb
+++ terminal_jobs.rb
@@ -4,6 +4,4 @@ require './terminal_jobs/tab'
 require './terminal_jobs/generate'
 
 Generate.new.generate_autorun
-puts "Autorun file generation complete!"
-puts "./autorun.sh to start your tasks..."
-puts ""
+puts "Autorun.sh successfully generated!!!"
