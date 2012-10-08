563f729d6bedc052704ffa56f1c543c964834d0d
shaun <davissp14@gmail.com>
2012-10-08 01:37:17 -0700

    Added some output on autorun generation

--- terminal_jobs.rb
+++ terminal_jobs.rb
@@ -4,3 +4,6 @@ require './terminal_jobs/tab'
 require './terminal_jobs/generate'
 
 Generate.new.generate_autorun
+puts "Autorun file generation complete!"
+puts "./autorun.sh to start your tasks..."
+puts ""
