36a0fb560e7e6d3fe1bcc18ce43d5822f12c2cb3
shaun <davissp14@gmail.com>
2012-10-08 09:54:40 -0700

    Added some additional basic tests.

--- test/generate_test.rb
+++ test/generate_test.rb
@@ -1,7 +1,14 @@
-require "terminal_jobs/generate"
+require "./terminal_jobs/generate"
 require "test/unit"
+require 'yaml'
 
 class TestGenerate < Test::Unit::TestCase
+  def setup
+    @gen = Generate.new
+  end
   
+  def test_initialize
+    assert_equal("Terminal", @gen.app)
+  end
   
 end
--- test/tab_test.rb
+++ test/tab_test.rb
@@ -8,7 +8,8 @@ class TabTest < Test::Unit::TestCase
   end
   
   def test_initialize
-    assert_equal("local_runner", @tab.name)
+    assert_equal("local_runner", @tab.name, "Name does not match.")
+    assert_equal("/Users/shaun/Programming/mongohq/probe-server", @tab.directive, "Directive does not match.")
   end
   
 end
